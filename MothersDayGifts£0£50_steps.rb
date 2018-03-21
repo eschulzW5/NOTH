e91d592006c4325821e09daafd5bb467412a2886
eschulzW5 <37583840+eschulzW5@users.noreply.github.com>
2018-03-21 14:47:14 +0000

    Create MothersDayGifts£0£50_steps.rb

---
+++ MothersDayGifts£0£50_steps.rb
@@ -0,0 +1,20 @@
+require 'selenium-webdriver'
+driver = Selenium::WebDriver.for :firefox
+
+Given(/^a customer naviagtes to the homepage$/) do
+  driver.navigate.to "http://notonthehighstreet.com/"
+end
+
+When(/^The customer enters ‘Mothers Day Gifts and £0-£50 and submits the request$/) do
+  driver.find_element(:id, 'term').send_keys("Mothers day gifts £0-£50")
+  driver.find_element(:name, 'search').click
+end
+
+Then(/^the relevant items are returned$/) do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
+  begin
+  	element = wait.until { driver.find_element(:id => "content") }
+  ensure
+    driver.quit
+  end
+end
