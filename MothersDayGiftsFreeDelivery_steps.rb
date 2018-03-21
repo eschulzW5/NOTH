414e0d6ad6e329227fc24897da5f003fdd25adc9
eschulzW5 <37583840+eschulzW5@users.noreply.github.com>
2018-03-21 14:01:14 +0000

    Update MothersDayGiftsFreeDelivery_steps.rb

--- MothersDayGiftsFreeDelivery_steps.rb
+++ MothersDayGiftsFreeDelivery_steps.rb
@@ -5,12 +5,12 @@ Given(/^a customer naviagtes to the homepage$/) do
   driver.navigate.to "http://notonthehighstreet.com/"
 end
 
-When(/^The customer enters ‘Mothers Day Gifts’ and selects ‘Free delivery’ and submits the request$/) do
+When(/^The customer enters ‘Mothers Day Gifts and free delivery and submits the request$/) do
   driver.find_element(:id, 'term').send_keys("Mothers day gifts and free delivery")
   driver.find_element(:name, 'search').click
 end
 
-Then(/^the relevant items are returned with the most appropriate products at the top$/) do
+Then(/^the relevant items are returned$/) do
   wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
   begin
   	element = wait.until { driver.find_element(:id => "content") }
