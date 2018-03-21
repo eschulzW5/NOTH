18356db8d0d4ac39c01ea7518c687fd4411fd4b1
eschulzW5 <37583840+eschulzW5@users.noreply.github.com>
2018-03-21 20:18:40 +0000

    Create ProductDeliveryInfo_steps.rb

---
+++ ProductDeliveryInfo_steps.rb
@@ -0,0 +1,19 @@
+require 'selenium-webdriver'
+driver = Selenium::WebDriver.for :firefox
+
+Given(/^a customer navigates to the product page$/) do
+  driver.navigate.to "https://www.notonthehighstreet.com/thisisnessie/product/personalised-wellies-family-print"
+end
+
+When(/^the customer selects delivery information$/) do
+  driver.find_element(:id, 'ga_delivery_tab_item').click
+end
+
+Then(/^the product delivery information is displayed$/) do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
+  begin
+  	element = wait.until { driver.find_element(:class => "ui-state-default delivery_tab ui-tabs-selected ui-state-active") }
+  ensure
+    driver.quit
+  end
+end
