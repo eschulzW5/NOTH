410b9324cc8a35ffabbc2a1855cc175352e3096b
eschulzW5 <37583840+eschulzW5@users.noreply.github.com>
2018-03-21 22:05:27 +0000

    Update MothersDayGifts£0£50_steps.rb

--- MothersDayGifts£0£50_steps.rb
+++ MothersDayGifts£0£50_steps.rb
@@ -7,13 +7,13 @@ end
 
 When(/^The customer enters ‘Mothers Day Gifts and £0-£50 and submits the request$/) do
   driver.find_element(:id, 'term').send_keys("Mothers day gifts £0-£50")
-  driver.find_element(:name, 'search').click
+  driver.find_element(:class_name, 'gc-form__field--submit).click
 end
 
 Then(/^the relevant items are returned$/) do
   wait = Selenium::WebDriver::Wait.new(:timeout => 5) # seconds
   begin
-  	element = wait.until { driver.find_element(:id => "content") }
+  	element = wait.until { driver.find_element(:id => "results") }
   ensure
     driver.quit
   end
